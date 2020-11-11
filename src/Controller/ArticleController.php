ef98f21fdfd632183e7594144248d53d70b04c30
Haashim <32544671+stravos97@users.noreply.github.com>
2020-11-11 22:57:53 +0000

    a
    a

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController {
+class ArticleController extends AbstractController {
     /**
      * @Route("/") 
      * @Method({"GET"})
      */ #instead of using routes.yaml and specificying the route there, we can do this
     public function index() {
-        return new Response('<h1>Hello<h1>');
+        // return new Response('<h1>Hello<h1>');
+        return $this->render('articles/index.html.twig');
     }
 
 }
