d00b46d6be92ae23594479112dd56b27b4413793
Haashim <32544671+stravos97@users.noreply.github.com>
2020-11-11 23:40:07 +0000

    Simple Twig Array
    Simple twig array showing my name, and rename of some files

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,14 +6,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController extends AbstractController {
+class IndexController extends AbstractController { //article controller
     /**
      * @Route("/") 
      * @Method({"GET"})
      */ #instead of using routes.yaml and specificying the route there, we can do this
     public function index() {
         // return new Response('<h1>Hello<h1>');
-        return $this->render('articles/index.html.twig');
+         return $this->render('articles/index.html.twig', array('name' => 'Hello')); //we use the array to pass stuff into the controller
     }
 
 }
