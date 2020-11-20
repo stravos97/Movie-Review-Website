96eec9ac04ac7504f3bfd338ff05ff39fa9f1a93
Haashim <32544671+stravos97@users.noreply.github.com>
2020-11-20 21:11:48 +0000

    Made local dB with Article table
    Has article table, and different fields related to Article content

---
+++ src/Entity/Article.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+//    public function getId(): ?int
+//    {
+//        return $this->id;
+//    }
+}
