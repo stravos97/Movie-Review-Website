12bd26971ae7bbe8cb3864d413b1ed7474ee66a3
haashimalvi@protonmail.com <haashimalvi@protonmail.com>
2020-12-02 00:58:30 +0000

    Added new ArticleFormType.php and updated navbar and IndexController.php. new article page broken.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,10 +1,14 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\Article;
 
 class IndexController extends AbstractController { //article controller
@@ -21,14 +25,42 @@ class IndexController extends AbstractController { //article controller
          return $this->render('articles/index.html.twig', array('articles' => $articles)); //we use this to generate article objects, each containing all of our article data
     }
 
+
+    /**
+     * @Route ("/article/new", name="new_article")
+     * @Method({"GET", "POST"})
+     */
+//    public function new(Request $request){
+//
+//        $article = new Article();
+//
+//        $form = $this->createFormBuilder($article)
+//            ->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+//            ->add('body', TextareaType::class, array(
+//                'required' => false,
+//                'attr' => array('class' => 'form-control')
+//            ))
+//            ->add('save', SubmitType::class, array(
+//                'label' => 'Create',
+//                'attr' => array('class' => 'btn btn-primary mt-3')
+//            ))
+//            ->getForm();
+//
+//        return $this->render('articles/new.html.twig', array(
+//            'form' => $form->createView()
+//        ));
+//
+//    }
+
     /**
      * @Route("/article/{id}", name="article_show")
-     */
+     */ //THIS should always be last. It will try to load anything after /article as an id, even if it is not an id
     public function show($id){ //gets the id from the {} above
         $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
         return $this->render('articles/show.html.twig', array('article' => $article)); //only contains a single article
     }
+
 
 //    /**
 //     * @Route ("/article/saves")
