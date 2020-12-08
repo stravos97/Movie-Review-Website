2ec4de5fbb4263679df9be9cdf7713fb8a9b944b
haashimalvi@protonmail.com <haashimalvi@protonmail.com>
2020-12-08 17:25:16 +0000

    fixed login authenticator that allows users, to use a password (ecrypted with bycvcrypt https://www.devglan.com/online-tools/bcrypt-hash-generator) to login password for user 1 is 123456789

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
@@ -17,17 +18,20 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator
 {
     private $userRepository;
     private $router;
+    private $passwordEncoder;
 
     /**
      * routerInterface is used for redirection. Instead of hardcoding a redirect we use RouterInterface
      * LoginAuthenticator constructor.
      * @param UserRepository $userRepository
      * @param RouterInterface $router
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
      */
-    public function __construct(UserRepository $userRepository, RouterInterface $router){
+    public function __construct(UserRepository $userRepository, RouterInterface $router, UserPasswordEncoderInterface $passwordEncoder){
         
         $this->userRepository = $userRepository;
         $this->router = $router;
+        $this->passwordEncoder = $passwordEncoder;
     }
     public function supports(Request $request)
     {
@@ -82,7 +86,8 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator
      */
     public function checkCredentials($credentials, UserInterface $user)
     {
-        return true;
+       // return true;
+        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
     }
 
     /**
