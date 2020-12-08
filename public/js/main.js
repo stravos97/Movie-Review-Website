7e8f92ee22e53ffac6dc55c9faf9abd386aceef2
haashimalvi@protonmail.com <haashimalvi@protonmail.com>
2020-12-08 17:55:23 +0000

    Added simple admin dashboard, and fixed delete button.
    Role isn't assgigned yet

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const articles = document.getElementById('articles')
 if (articles) {
     articles.addEventListener("click", (e) => {
 
-        if (e.target.className === 'btn btn-danger delete-article'){
+        if (e.target.className === 'btn btn-danger delete-article border-r' || e.target.className === 'btn btn-danger delete-article'){
 
             if (confirm('Are you sure?')){
                 const id = e.target.getAttribute('data-id');
